53d495cd86f5e0e857a8cf63d048253d19b8212d
cjn404 <moi404@naver.com>
2025-09-19 00:57:03 +0900

    [level6] feat: cascade 기능을 활용해 todo 등록 시 유저를 담당자 컬럼(assignee_id)에 자동 등록

--- src/main/java/org/example/expert/domain/todo/entity/Todo.java
+++ src/main/java/org/example/expert/domain/todo/entity/Todo.java
@@ -27,6 +27,14 @@ public class Todo extends Timestamped {
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
+    // 새 User 객체를 Todo에 save 시 담당자(assignee)로 자동 저장
+    // CascadeType.PERSIST 시 Detached entity 에러 발생
+    // 이유: 이미 영속 상태(Persistent)인 객체를 persist() 호출
+    // 해결: merge() 사용(DB에 이미 존재하는 영속 객체 또는 준영속 객체 저장)
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.MERGE)
+    @JoinColumn(name = "assignee_id", nullable = false)
+    private User assignee;
+
     @OneToMany(mappedBy = "todo", cascade = CascadeType.REMOVE)
     private List<Comment> comments = new ArrayList<>();
 
@@ -38,6 +46,7 @@ public class Todo extends Timestamped {
         this.contents = contents;
         this.weather = weather;
         this.user = user;
+        this.assignee = user;    // Todo 생성 시 담당자 자동 등록
         this.managers.add(new Manager(user, this));
     }
 }
