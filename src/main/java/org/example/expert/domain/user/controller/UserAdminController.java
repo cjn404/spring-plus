cbb9f7ebccf9ef30fac71e72fc2c4ab3613788b8
cjn404 <moi404@naver.com>
2025-09-19 12:00:48 +0900

    [level5] feat: AOP 변경 사항 확인 로그 및 출력문 추가(주석 처리)

--- src/main/java/org/example/expert/domain/user/controller/UserAdminController.java
+++ src/main/java/org/example/expert/domain/user/controller/UserAdminController.java
@@ -1,6 +1,7 @@
 package org.example.expert.domain.user.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.example.expert.domain.user.dto.request.UserRoleChangeRequest;
 import org.example.expert.domain.user.service.UserAdminService;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class UserAdminController {
@@ -16,6 +18,9 @@ public class UserAdminController {
 
     @PatchMapping("/admin/users/{userId}")
     public void changeUserRole(@PathVariable long userId, @RequestBody UserRoleChangeRequest userRoleChangeRequest) {
+        // AOP 변경 사항 확인 로그 및 출력문
+        // log.info(">>> changeUserRole() 실행");
+        // System.out.println(">>> changeUserRole() 실행");
         userAdminService.changeUserRole(userId, userRoleChangeRequest);
     }
 }
