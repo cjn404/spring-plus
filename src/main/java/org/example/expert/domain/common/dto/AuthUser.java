4cd187452cd6d63ba452a35404be3b817388cfea
cjn404 <moi404@naver.com>
2025-09-23 00:59:54 +0900

    [level9] fix: 누락된 userRole 필드 추가

--- src/main/java/org/example/expert/domain/common/dto/AuthUser.java
+++ src/main/java/org/example/expert/domain/common/dto/AuthUser.java
@@ -13,11 +13,13 @@ public class AuthUser {
 
     private final Long id;
     private final String email;
+    private final UserRole userRole;
     private final Collection<? extends GrantedAuthority> authorities;
 
     public AuthUser(Long id, String email, UserRole userRole) {
         this.id = id;
         this.email = email;
+        this.userRole = userRole;
         this.authorities = List.of(new SimpleGrantedAuthority(userRole.name()));
     }
 }
